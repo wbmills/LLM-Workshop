52236b222d141560cae5cc3717549c07092085ee
William Mills <93946961+wbmills@users.noreply.github.com>
2025-02-04 12:29:58 +0000

    adding utils.py contents to noteobook

--- utils.py
+++
@@ -1,9 +0,0 @@
-import fitz
-from PyPDF2 import PdfReader
-
-def extract_pdf(file):
-    reader = PdfReader(file)
-    text = "\n".join(page.extract_text() for page in reader.pages if page.extract_text())
-    if 'incorrect startxref pointer(1)' in text:
-        text = 'Error reading PDF - try different file'
-    return text
