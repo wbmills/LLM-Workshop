088132181a0b27632fe0e78149b7d6cecd79f7c5
William Mills <93946961+wbmills@users.noreply.github.com>
2025-02-04 11:56:22 +0000

    added basic struct

---
+++ utils.py
@@ -0,0 +1,8 @@
+import fitz
+
+def extract_text_from_pdf(pdf_path):
+    text = ""
+    with fitz.open(pdf_path) as doc:
+        for page in doc:
+            text += page.get_text("text")  # Extract text from each page
+    return text
