8cc30a41a978d9b5cafed57f75ecd6db375fbc5b
William Mills <93946961+wbmills@users.noreply.github.com>
2025-02-04 14:46:02 +0000

    formatted  csv

--- ChatPDF.py
+++ ChatPDF.py
@@ -11,8 +11,9 @@ class ChatPDF():
 
         self.pdf_loc = os.getenv("FILE_LOC")
         self.client = OpenAI(api_key=api_key)
+        self.thing_to_predict = ''
 
-    def call_chatgpt(self, system_prompt, user_prompt, output_response=True):
+    def call_chatgpt(self, system_prompt, user_prompt, output_response=False):
         response = self.client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
@@ -30,10 +31,11 @@ class ChatPDF():
 
     def __call__(self, thing_to_predict:str):
         pdf_list = self.read_pdfs()
+        self.thing_to_predict = thing_to_predict
         seperator = '\n-----\n'
         question = f"Take these sets of texts (sperated by {seperator}) and find key predictors (if any) of {thing_to_predict}"
 
-        format = 'Summarise your answer as a table, formatted with the column names "Paper Name", "Why it is helpful", "Predictive Feature". Also provide the same features under the header "FEATURES:"'
+        format = 'You summarise your answers as comma-deliminated tables fit for a csv (not a terminal), with no "|" values, with the column names "Paper Name", "Why it is helpful", "Predictive Feature".'
 
         prompt = f'{question} + \n\n'
         for key, value in pdf_list.items():
@@ -43,13 +45,16 @@ class ChatPDF():
         prompt += format
 
         gpt_response = self.call_chatgpt(system_prompt=format, user_prompt=prompt)
-        response, factors_extended = gpt_response.split('FEATURES:')
+
+        with open('file.csv', 'w') as f:
+            f.write(gpt_response)
+
+        features_reduced = self.call_chatgpt(
+            "You summarise text into between 1 and 3 keywords. You provide the keywords and nothing more.",
+            'Sumarise the features column of this: ' + gpt_response)
         
-        features_reduced = self.call_chatgpt("You summarise text into between 1 and 3 keywords. You provide the keywords and nothing more.",
-                                             factors_extended)
-        
-        print(response)
         print(features_reduced)
+        return gpt_response
     
     def extract_pdf(self, file):
         reader = PdfReader(file)
@@ -71,6 +76,22 @@ class ChatPDF():
 
         return pdf_files
     
+    def call_dalle3(self, given_table):
+        assert self.thing_to_predict is not None, 'call ChatPDF and set a thing to predict first!'
+        
+        prompt = self.call_chatgpt(system_prompt="you provide a summary of these features in less than 400 characters (no table). Create it as a prompt for dalle, where the aim is to get a nice image or cartoon of the thing I am trying to predict. ", user_prompt=given_table)
+        prompt += 'This image has no words or letters. It is artistic. It is in the style of jackson polloc'
+        response = self.client.images.generate(
+            model="dall-e-2",
+            prompt=prompt,
+            size="512x512",
+            quality="standard",
+            n=1,
+        )
+
+        print(response.data[0].url)
+    
 
 gpt = ChatPDF()
-gpt('Ecosystem health')
+table = gpt('Ecosystem health')
+# gpt.call_dalle3(table)
