6adb97f762738e48b345428eb08d11c22ff9290d
William Mills <93946961+wbmills@users.noreply.github.com>
2025-02-04 12:29:16 +0000

    creating pdf

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import fitz
+from PyPDF2 import PdfReader
 
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    with fitz.open(pdf_path) as doc:
-        for page in doc:
-            text += page.get_text("text")  # Extract text from each page
+def extract_pdf(file):
+    reader = PdfReader(file)
+    text = "\n".join(page.extract_text() for page in reader.pages if page.extract_text())
+    if 'incorrect startxref pointer(1)' in text:
+        text = 'Error reading PDF - try different file'
     return text
